5a4dfcf2ad3e248d2f5c53e73324ca6d5911e90d
Yasen Dimitrov <y_dimitrov@ymail.com>
2024-02-20 22:35:24 +0200

    nvim: misc

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -3,3 +3,6 @@ vim.o.swapfile = false -- disable swapfile
 vim.opt.updatetime = 100 -- faster completion (4000ms default)
 vim.opt.timeoutlen = 200
 --vim.opt.winbar = "%=%m  %f"
+
+-- Concealer for Obsidian
+vim.opt.conceallevel = 2
