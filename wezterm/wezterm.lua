54e996eae26437864c28334868fedb73aff5c06b
Yasen Dimitrov <y_dimitrov@ymail.com>
2024-02-20 22:35:39 +0200

    wezterm: zenmode update

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -15,7 +15,7 @@ config.font = wezterm.font_with_fallback({
 })
 --config.color_scheme = "Windows 10 (base16)"
 config.color_scheme = "Catppuccin Mocha"
-config.window_background_opacity = 1
+config.window_background_opacity = 1.0
 config.macos_window_background_blur = 30
 config.window_padding = { left = 5, right = 5, top = 10, bottom = 0 }
 config.line_height = 1.10
@@ -44,5 +44,29 @@ config.tab_bar_at_bottom = true
 config.hide_tab_bar_if_only_one_tab = true
 config.tab_max_width = 32
 config.unzoom_on_switch_pane = true
+
+-- zenmode; ref: https://github.com/folke/zen-mode.nvim
+wezterm.on("user-var-changed", function(window, pane, name, value)
+	local overrides = window:get_config_overrides() or {}
+	if name == "ZEN_MODE" then
+		local incremental = value:find("+")
+		local number_value = tonumber(value)
+		if incremental ~= nil then
+			while number_value > 0 do
+				window:perform_action(wezterm.action.IncreaseFontSize, pane)
+				number_value = number_value - 1
+			end
+			overrides.enable_tab_bar = false
+		elseif number_value < 0 then
+			window:perform_action(wezterm.action.ResetFontSize, pane)
+			overrides.font_size = nil
+			overrides.enable_tab_bar = true
+		else
+			overrides.font_size = number_value
+			overrides.enable_tab_bar = false
+		end
+	end
+	window:set_config_overrides(overrides)
+end)
 
 return config
