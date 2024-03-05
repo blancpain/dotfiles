da928fddb8db524c8d55554971281237a6a94885
Yasen Dimitrov <y_dimitrov@ymail.com>
2024-03-05 23:56:24 +0200

    nvim: minor options tweaks

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,3 +2,7 @@ vim.opt.list = false
 vim.o.swapfile = false -- disable swapfile
 vim.opt.updatetime = 100 -- faster completion (4000ms default)
 vim.opt.timeoutlen = 200
+vim.opt.scrolloff = 10
+
+-- Add asterisks in block comments
+vim.opt.formatoptions:append({ "r" }) -- WARN: doesn't work as expected
