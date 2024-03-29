fb56da43debcf31156a5dbf34c8a3e8a2a4e9a42
Yasen Dimitrov <y_dimitrov@ymail.com>
2024-03-29 23:52:14 +0200

    nvim: update to harpoon2

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -1,14 +1,105 @@
+-- NOTE: use below if we want to use telescope as a harpoon menu
+
+local conf = require("telescope.config").values
+local function toggle_telescope(harpoon_files)
+  local file_paths = {}
+  for _, item in ipairs(harpoon_files.items) do
+    table.insert(file_paths, item.value)
+  end
+
+  require("telescope.pickers")
+    .new({}, {
+      prompt_title = "Harpoon",
+      finder = require("telescope.finders").new_table({
+        results = file_paths,
+      }),
+      previewer = conf.file_previewer({}),
+      sorter = conf.generic_sorter({}),
+    })
+    :find()
+end
+
 return {
   "ThePrimeagen/harpoon",
-  lazy = false,
-  dependencies = {
-    "nvim-lua/plenary.nvim",
+  branch = "harpoon2",
+  -- NOTE: optionally add plenary and telescope (if using telescope for menu) as dependencies...
+  opts = {
+    menu = {
+      width = vim.api.nvim_win_get_width(0) - 4,
+    },
   },
-  config = true,
   keys = {
-    { "<s-m>", "<cmd>lua require('harpoon.mark').add_file()<cr>", desc = "Mark file with harpoon" },
-    { "<leader>j", "<cmd>lua require('harpoon.ui').nav_next()<cr>", desc = "Go to next harpoon mark" },
-    { "<leader>k", "<cmd>lua require('harpoon.ui').nav_prev()<cr>", desc = "Go to previous harpoon mark" },
-    { "<TAB>", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>", desc = "Show harpoon marks" },
+    {
+      "<s-m>",
+      function()
+        require("harpoon"):list():append()
+      end,
+      desc = "Mark file with Harpoon",
+    },
+    {
+      "<TAB>",
+      function()
+        local harpoon = require("harpoon")
+        harpoon.ui:toggle_quick_menu(harpoon:list())
+      end,
+      desc = "Harpoon quick menu",
+    },
+    {
+      "<leader>h",
+      function()
+        local harpoon = require("harpoon")
+        toggle_telescope(harpoon:list())
+      end,
+      desc = "Harpoon telescope",
+    },
+    {
+      "<leader>j",
+      function()
+        require("harpoon"):list():next()
+      end,
+      desc = "Go to next harpoon mark",
+    },
+    {
+      "<leader>k",
+      function()
+        require("harpoon"):list():prev()
+      end,
+      desc = "Go to previous harpoon mark",
+    },
+    {
+      "<leader>1",
+      function()
+        require("harpoon"):list():select(1)
+      end,
+      desc = "Harpoon to file 1",
+    },
+    {
+      "<leader>2",
+      function()
+        require("harpoon"):list():select(2)
+      end,
+      desc = "Harpoon to file 2",
+    },
+    {
+      "<leader>3",
+      function()
+        require("harpoon"):list():select(3)
+      end,
+      desc = "Harpoon to file 3",
+    },
+    {
+      "<leader>4",
+      function()
+        require("harpoon"):list():select(4)
+      end,
+      desc = "Harpoon to file 4",
+    },
+    {
+      "<leader>5",
+      function()
+        require("harpoon"):list():select(5)
+      end,
+      desc = "Harpoon to file 5",
+    },
   },
 }
