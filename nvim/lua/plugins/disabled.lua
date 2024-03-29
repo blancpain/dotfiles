d5fd9ea2325a81bd9b11464e273cee62bfdc98cf
Yasen Dimitrov <y_dimitrov@ymail.com>
2024-03-29 23:52:32 +0200

    nvim: disable treesitter context as laggy in tmux

--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -7,4 +7,5 @@ return {
   { "rcarriga/nvim-notify", enabled = false },
   -- { "lukas-reineke/headlines.nvim", enabled = false },
   { "echasnovski/mini.comment", enabled = false },
+  { "nvim-treesitter/nvim-treesitter-context", enabled = false },
 }
