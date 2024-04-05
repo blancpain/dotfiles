dd7e8f932b4c821e54b0fd60aaf248d0dd0d14c1
Yasen Dimitrov <y_dimitrov@ymail.com>
2024-04-05 18:35:26 +0300

    nvim: add gitsings mappings and minor tweaks

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -1,23 +1,62 @@
 return {
-  "NeogitOrg/neogit",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "sindrets/diffview.nvim",
-  },
-  cmd = "Neogit",
-  opts = {
-    signs = {
-      hunk = { "", "" },
-      item = { "", "" },
-      section = { "", "" },
+  {
+    "NeogitOrg/neogit",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "sindrets/diffview.nvim",
     },
-    integrations = {
-      diffview = true,
+    cmd = "Neogit",
+    opts = {
+      signs = {
+        hunk = { "", "" },
+        item = { "", "" },
+        section = { "", "" },
+      },
+      integrations = {
+        diffview = true,
+      },
+      kind = "vsplit",
     },
-    kind = "vsplit",
+    keys = {
+      { "<leader>gm", "<cmd>lua require('neogit').open({'commit'})<CR>", desc = "Neogit commit" },
+      { "<leader>gS", "<cmd>lua require('neogit').open()<CR>", desc = "Neogit Status" },
+    },
   },
-  keys = {
-    { "<leader>gm", "<cmd>lua require('neogit').open({'commit'})<CR>", desc = "Neogit commit" },
-    { "<leader>gS", "<cmd>lua require('neogit').open()<CR>", desc = "Neogit Status" },
+  {
+    "lewis6991/gitsigns.nvim",
+    event = "LazyFile",
+    opts = {
+      signs = {
+        add = { text = "▎" },
+        change = { text = "▎" },
+        delete = { text = "" },
+        topdelete = { text = "" },
+        changedelete = { text = "▎" },
+        untracked = { text = "▎" },
+      },
+      current_line_blame = true,
+      on_attach = function(buffer)
+        local gs = package.loaded.gitsigns
+
+        local function map(mode, l, r, desc)
+          vim.keymap.set(mode, l, r, { buffer = buffer, desc = desc })
+        end
+
+      -- stylua: ignore start
+      map("n", "]h", gs.next_hunk, "Next Hunk")
+      map("n", "[h", gs.prev_hunk, "Prev Hunk")
+      map({ "n", "v" }, "<leader>ghs", ":Gitsigns stage_hunk<CR>", "Stage Hunk")
+      map({ "n", "v" }, "<leader>ghr", ":Gitsigns reset_hunk<CR>", "Reset Hunk")
+      map("n", "<leader>ghS", gs.stage_buffer, "Stage Buffer")
+      map("n", "<leader>ghu", gs.undo_stage_hunk, "Undo Stage Hunk")
+      map("n", "<leader>ghR", gs.reset_buffer, "Reset Buffer")
+      map("n", "<leader>ghp", gs.preview_hunk_inline, "Preview Hunk Inline")
+      map("n", "<leader>ghb", function() gs.blame_line({ full = true }) end, "Blame Line")
+      map("n", "<leader>ghd", gs.diffthis, "Diff This")
+      map("n", "<leader>ghD", function() gs.diffthis("~") end, "Diff This ~")
+      map("n", "<leader>ght", gs.toggle_current_line_blame, "Toggle Current Line Blame")
+      map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", "GitSigns Select Hunk")
+      end,
+    },
   },
 }
