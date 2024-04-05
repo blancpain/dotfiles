e45fa35b4d42f8740d400fbcfac97c01766090c7
Yasen Dimitrov <y_dimitrov@ymail.com>
2024-04-05 11:54:57 +0300

    nvim: add neogit, rm vim-fugitive

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -1,1 +1,24 @@
-return { "tpope/vim-fugitive" }
+-- return { "tpope/vim-fugitive" }
+
+return {
+  "NeogitOrg/neogit",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "sindrets/diffview.nvim",
+  },
+  cmd = "Neogit",
+  opts = {
+    signs = {
+      hunk = { "", "" },
+      item = { "", "" },
+      section = { "", "" },
+    },
+    integrations = {
+      diffview = true,
+    },
+  },
+  -- keys = {
+  --   { "<leader>gc", "<cmd>lua require('neogit').open({'commit'})<CR>", desc = "Git commit" },
+  --   { "<leader>gg", "<cmd>lua require('neogit').open()<CR>", desc = "Git commit" },
+  -- },
+}
