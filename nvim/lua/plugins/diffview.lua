a0a6a0ecb09de4fe07c8f690a3f1438c00120be0
Yasen Dimitrov <y_dimitrov@ymail.com>
2024-04-05 11:54:40 +0300

    nvim: add diffview

---
+++ nvim/lua/plugins/diffview.lua
@@ -0,0 +1,22 @@
+return {
+  "sindrets/diffview.nvim",
+  dependencies = "nvim-lua/plenary.nvim",
+  cmd = {
+    "DiffviewOpen",
+    "DiffviewClose",
+    "DiffviewToggleFiles",
+    "DiffviewFocusFiles",
+    "DiffviewRefresh",
+    "DiffviewFileHistory",
+  },
+  opts = {
+    file_panel = {
+      position = "bottom",
+      height = 20,
+    },
+  },
+  keys = {
+    { "<leader>gd", "<cmd>DiffviewOpen<cr>", desc = "DiffviewOpen" },
+    { "<leader>gD", "<cmd>DiffviewClose<cr>", desc = "DiffviewClose" },
+  },
+}
