a6b6673a2d1df4644487d5085d610d05b4283669
Yasen Dimitrov <y_dimitrov@ymail.com>
2024-04-09 11:51:23 +0300

    nvim: replace markdowny w/ markdown.nvim

--- nvim/lua/plugins/markdown.lua
+++ nvim/lua/plugins/markdown.lua
@@ -1,6 +1,19 @@
 return {
-  "antonk52/markdowny.nvim",
-  config = function()
-    require("markdowny").setup()
-  end,
+  "tadmccorkle/markdown.nvim",
+  ft = "markdown", -- or 'event = "VeryLazy"'
+  opts = {
+    -- configuration here or empty for defaults
+    mappings = {
+      inline_surround_toggle = "gms", -- (string|boolean) toggle inline style
+      inline_surround_toggle_line = "gmss", -- (string|boolean) line-wise toggle inline style
+      inline_surround_delete = "ds", -- (string|boolean) delete emphasis surrounding cursor
+      inline_surround_change = "cs", -- (string|boolean) change emphasis surrounding cursor
+      link_add = "gl", -- (string|boolean) add link
+      link_follow = false, -- disable
+      go_curr_heading = "]c", -- (string|boolean) set cursor to current section heading
+      go_parent_heading = "]p", -- (string|boolean) set cursor to parent section heading
+      go_next_heading = "]]", -- (string|boolean) set cursor to next section heading
+      go_prev_heading = "[[", -- (string|boolean) set cursor to previous section heading
+    },
+  },
 }
