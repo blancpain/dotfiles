fb92f670407155a507fa52c0864d02e088385ee0
Yasen Dimitrov <y_dimitrov@ymail.com>
2024-05-13 16:48:10 +0300

    nvim: add testing adapters - just and vitest

--- nvim/lua/plugins/testing.lua
+++ nvim/lua/plugins/testing.lua
@@ -3,36 +3,21 @@ return {
   dependencies = {
     "nvim-neotest/neotest-jest",
     "nvim-lua/plenary.nvim",
+    "haydenmeade/neotest-jest",
+    "marilari88/neotest-vitest",
   },
-  opts = {
-    -- Can be a list of adapters like what neotest expects,
-    -- or a list of adapter names,
-    -- or a table of adapter names, mapped to adapter configs.
-    -- The adapter will then be automatically loaded with the config.
-    adapters = {
-      ["neotest-jest"] = {
-        -- below for monorepos as per https://github.com/nvim-neotest/neotest-jest
-        -- jestConfigFile = function()
-        --   local file = vim.fn.expand("%:p")
-        --   if string.find(file, "/packages/") then
-        --     return string.match(file, "(.-/[^/]+/)src") .. "jest.config.ts"
-        --   end
-        --   return vim.fn.getcwd() .. "/jest.config.ts"
-        -- end,
-        -- cwd = function()
-        --   local file = vim.fn.expand("%:p")
-        --   if string.find(file, "/packages/") then
-        --     return string.match(file, "(.-/[^/]+/)src")
-        --   end
-        --   return vim.fn.getcwd()
-        -- end,
-      },
-    },
-    -- Example for loading neotest-go with a custom config
-    -- adapters = {
-    --   ["neotest-go"] = {
-    --     args = { "-tags=integration" },
-    --   },
-    -- },
-  },
+  opts = function(_, opts)
+    table.insert(
+      opts.adapters,
+      require("neotest-jest")({
+        jestCommand = "npm test --",
+        -- jestConfigFile = "jest.config.js",
+        env = { CI = true },
+        cwd = function()
+          return vim.fn.getcwd()
+        end,
+      })
+    )
+    table.insert(opts.adapters, require("neotest-vitest"))
+  end,
 }
