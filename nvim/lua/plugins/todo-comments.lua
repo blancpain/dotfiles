bb87b87d009942504b03b2eb3d547e32ba4e9126
Yasen Dimitrov <y_dimitrov@ymail.com>
2024-05-13 16:48:24 +0300

    nvim: add new todo comments flags

--- nvim/lua/plugins/todo-comments.lua
+++ nvim/lua/plugins/todo-comments.lua
@@ -4,18 +4,42 @@ return {
   event = "LazyFile",
   config = true,
   -- stylua: ignore
+  opts = {
+    keywords = {
+      Q = {
+        icon = " ", 
+        color = "info", -- can be a hex color, or a named color (see below)
+        alt = { "FIXME", "BUG", "FIXIT", "ISSUE" }, -- a set of other keywords that all map to this FIX keywords
+        -- signs = false, -- configure signs for some keywords individually
+      },
+    },
+  },
   keys = {
-    { "]t", function() require("todo-comments").jump_next() end, desc = "Next Todo Comment" },
-    { "[t", function() require("todo-comments").jump_prev() end, desc = "Previous Todo Comment" },
+    {
+      "]t",
+      function()
+        require("todo-comments").jump_next()
+      end,
+      desc = "Next Todo Comment",
+    },
+    {
+      "[t",
+      function()
+        require("todo-comments").jump_prev()
+      end,
+      desc = "Previous Todo Comment",
+    },
     -- trouble
     { "<leader>xt", "<cmd>TodoTrouble<cr>", desc = "Todo (Trouble)" },
     { "<leader>xT", "<cmd>TodoTrouble keywords=TODO,FIX,FIXME<cr>", desc = "Todo/Fix/Fixme (Trouble)" },
     { "<leader>xF", "<cmd>TodoTrouble keywords=FIX,FIXME<cr>", desc = "Fix/Fixme (Trouble)" },
     { "<leader>xN", "<cmd>TodoTrouble keywords=NOTE<cr>", desc = "Note (Trouble)" },
+    { "<leader>xU", "<cmd>TodoTrouble keywords=Q<cr>", desc = "Questions (Trouble)" },
     -- telescope
     { "<leader>st", "<cmd>TodoTelescope<cr>", desc = "Todo" },
     { "<leader>sT", "<cmd>TodoTelescope keywords=TODO,FIX,FIXME<cr>", desc = "Todo/Fix/Fixme" },
     { "<leader>sF", "<cmd>TodoTelescope keywords=FIX,FIXME<cr>", desc = "Fix/Fixme (Trouble)" },
     { "<leader>sN", "<cmd>TodoTelescope keywords=NOTE<cr>", desc = "Note" },
+    { "<leader>sU", "<cmd>TodoTelescope keywords=Q<cr>", desc = "Questions" },
   },
 }
