a257e7566ed9f39d69fd286144c6f2a390edafba
Yasen Dimitrov <y_dimitrov@ymail.com>
2024-05-17 10:35:59 +0300

    nvim: add mini-surround extra following 0.10 release

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -23,7 +23,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
     { import = "lazyvim.plugins.extras.test.core" },
     { import = "lazyvim.plugins.extras.editor.outline" },
-
+    { import = "lazyvim.plugins.extras.coding.mini-surround" },
     { import = "plugins" },
   },
   defaults = {
