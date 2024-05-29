8bc2a80e8e8b094df9a9d8b8ead26b0ca85a99b8
Yasen Dimitrov <y_dimitrov@ymail.com>
2024-05-29 10:36:49 +0300

    nvim: update cmp with native snippets support

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -1,79 +1,66 @@
 return {
-  {
-    "hrsh7th/nvim-cmp",
-    dependencies = {
-      {
-        "hrsh7th/cmp-nvim-lsp",
-        event = "InsertEnter",
-      },
-      {
-        "hrsh7th/cmp-emoji",
-        event = "InsertEnter",
-      },
-      {
-        "hrsh7th/cmp-buffer",
-        event = "InsertEnter",
-      },
-      {
-        "hrsh7th/cmp-cmdline",
-        event = "InsertEnter",
-      },
-      {
-        "saadparwaiz1/cmp_luasnip",
-        event = "InsertEnter",
-      },
-      {
-        "hrsh7th/cmp-nvim-lua",
-      },
+  "hrsh7th/nvim-cmp",
+
+  dependencies = {
+    {
+      "hrsh7th/cmp-emoji",
+      event = "InsertEnter",
     },
-    ---@param opts cmp.ConfigSchema
-    opts = function(_, opts)
-      local cmp = require("cmp")
-
-      -- add emoji source
-      table.insert(opts.sources, { name = "emoji" })
-      vim.api.nvim_set_hl(0, "CmpItemKindEmoji", { fg = "#FDE030" })
-
-      vim.api.nvim_set_hl(0, "CmpItemKindCopilot", { fg = "#6CC644" })
-
-      local check_backspace = function()
-        local col = vim.fn.col(".") - 1
-        return col == 0 or vim.fn.getline("."):sub(col, col):match("%s")
-      end
-
-      opts.mapping = vim.tbl_extend("force", opts.mapping, {
-        ["<Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_next_item()
-            -- elseif require("copilot.suggestion").is_visible() then
-            --   require("copilot.suggestion").accept()
-          elseif vim.snippet.active({ direction = 1 }) then
-            vim.schedule(function()
-              vim.snippet.jump(1)
-            end)
-          elseif check_backspace() then
-            fallback()
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
-        ["<S-Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_prev_item()
-          elseif vim.snippet.active({ direction = -1 }) then
-            vim.schedule(function()
-              vim.snippet.jump(-1)
-            end)
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
-
-        -- change some default keymaps
-        ["<C-j>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
-        ["<C-k>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
-      })
-      opts.experimental.ghost_text = false
-    end,
+    {
+      "hrsh7th/cmp-cmdline",
+      event = "InsertEnter",
+    },
+    {
+      "hrsh7th/cmp-nvim-lua",
+    },
   },
+  ---@param opts cmp.ConfigSchema
+  opts = function(_, opts)
+    local cmp = require("cmp")
+
+    -- add emoji source
+    table.insert(opts.sources, { name = "emoji" })
+    vim.api.nvim_set_hl(0, "CmpItemKindEmoji", { fg = "#FDE030" })
+
+    vim.api.nvim_set_hl(0, "CmpItemKindCopilot", { fg = "#6CC644" })
+
+    local check_backspace = function()
+      local col = vim.fn.col(".") - 1
+      return col == 0 or vim.fn.getline("."):sub(col, col):match("%s")
+    end
+
+    opts.mapping = vim.tbl_extend("force", opts.mapping, {
+      ["<Tab>"] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_next_item()
+          -- elseif require("copilot.suggestion").is_visible() then
+          --   require("copilot.suggestion").accept()
+        elseif vim.snippet.active({ direction = 1 }) then
+          vim.schedule(function()
+            vim.snippet.jump(1)
+          end)
+        elseif check_backspace() then
+          fallback()
+        else
+          fallback()
+        end
+      end, { "i", "s" }),
+      ["<S-Tab>"] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_prev_item()
+        elseif vim.snippet.active({ direction = -1 }) then
+          vim.schedule(function()
+            vim.snippet.jump(-1)
+          end)
+        else
+          fallback()
+        end
+      end, { "i", "s" }),
+
+      -- change some default keymaps
+      ["<C-j>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+      ["<C-k>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+    })
+    opts.experimental.ghost_text = false
+  end,
 }
