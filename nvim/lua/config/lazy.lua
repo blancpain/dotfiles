753bddba1b6ae7cbc24d601d18d88448519568cd
Yasen Dimitrov <y_dimitrov@ymail.com>
2024-06-06 10:41:13 +0300

    nvim: use inc-rename extras, rm ufo as native fold is OK

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -26,6 +26,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.coding.mini-surround" },
     { import = "lazyvim.plugins.extras.ui.mini-indentscope" },
     { import = "lazyvim.plugins.extras.coding.mini-comment" },
+    { import = "lazyvim.plugins.extras.editor.inc-rename" },
     { import = "plugins" },
   },
   defaults = {
