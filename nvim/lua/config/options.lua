f1f87830b7fda8ce385f3e3a17ccbf803c4f655f
Yasen Dimitrov <y_dimitrov@ymail.com>
2024-08-14 10:48:08 +0300

    nvim: set background to dark

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -3,5 +3,7 @@ vim.o.swapfile = false -- disable swapfile
 vim.opt.updatetime = 100 -- faster completion (4000ms default)
 vim.opt.timeoutlen = 300
 
+vim.o.background = "dark" -- dark vs light
+
 -- Add asterisks in block comments
 vim.opt.formatoptions:append({ "r" }) -- WARN: doesn't work as expected
