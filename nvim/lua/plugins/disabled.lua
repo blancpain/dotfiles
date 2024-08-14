b0d88e5506fd03a4828f367158ad01705246cf2c
Yasen Dimitrov <y_dimitrov@ymail.com>
2024-08-14 10:50:24 +0300

    nvim: disable/rm unused plugins

--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -1,14 +1,9 @@
 return {
   { "akinsho/bufferline.nvim", enabled = false },
-  -- {
-  --   "hrsh7th/cmp-path",
-  --   enabled = false,
-  -- },
   { "rcarriga/nvim-notify", enabled = false },
-  -- { "lukas-reineke/headlines.nvim", enabled = false },
   { "echasnovski/mini.comment", enabled = false },
   { "NeogitOrg/neogit", enabled = false },
   { "folke/ts-comments.nvim", enabled = false },
-  -- { "nvim-telescope/telescope.nvim", enabled = false },
-  -- { "LunarVim/breadcrumbs.nvim", enabled = false },
+  { "LunarVim/breadcrumbs.nvim", enabled = false },
+  { "nvim-pack/nvim-spectre", enabled = false },
 }
