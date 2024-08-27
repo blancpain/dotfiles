9ce944e3d5b5bedb051310dd1b6667a55cca2816
Yasen Dimitrov <y_dimitrov@ymail.com>
2024-08-27 16:27:25 +0300

    nvim: use correct grug api to search and replace in current buffer

--- nvim/lua/plugins/grug.lua
+++ nvim/lua/plugins/grug.lua
@@ -2,28 +2,11 @@ return {
   "MagicDuck/grug-far.nvim",
   keys = {
     {
-      "<leader>sr",
-      function()
-        local is_visual = vim.fn.mode():lower():find("v")
-        if is_visual then -- needed to make visual selection work
-          vim.cmd([[normal! v]])
-        end
-        local grug = require("grug-far")
-        local ext = vim.bo.buftype == "" and vim.fn.expand("%:e")
-        local filesFilter = ext and ext ~= "" and "*." .. ext or nil; -- check if we actually needs this - it prefills the file type
-        (is_visual and grug.with_visual_selection or grug.grug_far)({
-          prefills = { filesFilter = filesFilter },
-        })
-      end,
-      mode = { "n", "v" },
-      desc = "Search and Replace",
-    },
-    {
       "<leader>sz",
       function()
         local grug = require("grug-far")
         grug.grug_far({
-          prefills = { flags = vim.fn.expand("%") },
+          prefills = { paths = vim.fn.expand("%") },
         })
       end,
       desc = "Search and Replace in current buffer",
