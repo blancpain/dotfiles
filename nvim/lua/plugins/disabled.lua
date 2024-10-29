4190539802753a760d81c486b350a4d0fc2ad7b9
Yasen Dimitrov <y_dimitrov@ymail.com>
2024-10-29 12:23:08 +0200

    nvim: disable multicursor

--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -4,4 +4,5 @@ return {
   { "echasnovski/mini.comment", enabled = false },
   { "NeogitOrg/neogit", enabled = false },
   { "folke/ts-comments.nvim", enabled = false },
+  { "jake-stewart/multicursor.nvim", enabled = false }, -- TODO: enable once more stable/mature
 }
