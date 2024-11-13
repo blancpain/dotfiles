6af1a1711073257e31d136cf19e3395c79e8c579
Yasen Dimitrov <y_dimitrov@ymail.com>
2024-11-13 17:33:23 +0200

    nvim: add Avante to md filetypes

--- nvim/lua/plugins/markdown.lua
+++ nvim/lua/plugins/markdown.lua
@@ -1,10 +1,18 @@
 return {
   "MeanderingProgrammer/render-markdown.nvim",
   opts = {
+    file_types = {
+      "markdown",
+      "norg",
+      "rmd",
+      "org",
+      "Avante",
+    },
     bullet = {
       -- padding
       left_pad = 0,
       right_pad = 1,
     },
   },
+  ft = { "markdown", "norg", "rmd", "org", "Avante" },
 }
