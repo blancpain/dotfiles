2fbca9ef9d585c1371683896dc875a7e50267e89
Yasen Dimitrov <y_dimitrov@ymail.com>
2024-11-13 17:33:04 +0200

    nvim: add avante, blink and snacks configs. Disable blink for now

---
+++ nvim/lua/plugins/blink.lua
@@ -0,0 +1,24 @@
+return {
+  "saghen/blink.cmp",
+
+  ---@module 'blink.cmp'
+  ---@type blink.cmp.Config
+  opts = {
+    keymap = {
+      ["<C-k>"] = { "select_prev", "fallback" },
+      ["<C-j>"] = { "select_next", "fallback" },
+      ["<Tab>"] = {
+        function(cmp)
+          if cmp.is_in_snippet() then
+            return cmp.accept()
+          else
+            return cmp.select_and_accept()
+          end
+        end,
+        "snippet_forward",
+        "fallback",
+      },
+      ["<S-Tab>"] = { "snippet_backward", "fallback" },
+    },
+  },
+}
--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -1,8 +1,8 @@
 return {
   { "akinsho/bufferline.nvim", enabled = false },
-  { "rcarriga/nvim-notify", enabled = false },
   { "echasnovski/mini.comment", enabled = false },
   { "NeogitOrg/neogit", enabled = false },
   { "folke/ts-comments.nvim", enabled = false },
   { "jake-stewart/multicursor.nvim", enabled = false }, -- TODO: enable once more stable/mature
+  { "saghen/blink.cmp", enabled = false },
 }
