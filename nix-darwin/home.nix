6a7e027c48e92306b317385d534b77d742e4f503
Yasen Dimitrov <y_dimitrov@ymail.com>
2024-11-17 16:54:33 +0200

    nix: home.nix cleanup

--- nix-darwin/home.nix
+++ nix-darwin/home.nix
@@ -5,10 +5,6 @@
   ...
 }:
 
-let
-  # Define the path to your dotfiles directory
-  dotfilesPath = "/Users/blancpain/dotfiles";
-in
 {
   home.username = "blancpain";
   home.homeDirectory = "/Users/blancpain";
@@ -16,33 +12,17 @@ in
 
   home.packages = [ ];
 
-  # NOTE: old
-
-  #   # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  #   # plain files is through 'home.file'.
-  #   home.file = {
-  #     ".config/wezterm".source = ../wezterm;
-  #     ".config/starship".source = ../starship;
-  #     ".config/nvim".source = ../nvim;
-  #     ".config/nix".source = ../nix;
-  #     ".config/nix-darwin".source = ../nix-darwin;
-  #     ".config/tmux".source = ../tmux;
-  #     ".config/lazygit".source = ../lazygit;
-  #     ".config/aerospace".source = ../aerospace;
-  #     ".config/nushell".source = ../nushell;
-  #   };
-
   home.file = {
-    ".config/wezterm".source = config.lib.file.mkOutOfStoreSymlink "${dotfilesPath}/wezterm";
-    ".config/starship".source = config.lib.file.mkOutOfStoreSymlink "${dotfilesPath}/starship";
-    ".config/nvim".source = config.lib.file.mkOutOfStoreSymlink "${dotfilesPath}/nvim";
-    ".config/nix".source = config.lib.file.mkOutOfStoreSymlink "${dotfilesPath}/nix";
-    ".config/nix-darwin".source = config.lib.file.mkOutOfStoreSymlink "${dotfilesPath}/nix-darwin";
-    ".config/tmux".source = config.lib.file.mkOutOfStoreSymlink "${dotfilesPath}/tmux";
-    ".config/lazygit".source = config.lib.file.mkOutOfStoreSymlink "${dotfilesPath}/lazygit";
-    ".config/aerospace".source = config.lib.file.mkOutOfStoreSymlink "${dotfilesPath}/aerospace";
-    ".config/nushell".source = config.lib.file.mkOutOfStoreSymlink "${dotfilesPath}/nushell";
-    ".config/fish".source = config.lib.file.mkOutOfStoreSymlink "${dotfilesPath}/fish";
+    ".config/wezterm".source = config.lib.file.mkOutOfStoreSymlink ../wezterm;
+    ".config/starship".source = config.lib.file.mkOutOfStoreSymlink ../starship;
+    ".config/nvim".source = config.lib.file.mkOutOfStoreSymlink ../nvim;
+    ".config/nix".source = config.lib.file.mkOutOfStoreSymlink ../nix;
+    ".config/nix-darwin".source = config.lib.file.mkOutOfStoreSymlink ../nix-darwin;
+    ".config/tmux".source = config.lib.file.mkOutOfStoreSymlink ../tmux;
+    ".config/lazygit".source = config.lib.file.mkOutOfStoreSymlink ../lazygit;
+    ".config/aerospace".source = config.lib.file.mkOutOfStoreSymlink ../aerospace;
+    ".config/nushell".source = config.lib.file.mkOutOfStoreSymlink ../nushell;
+    ".config/fish".source = config.lib.file.mkOutOfStoreSymlink ../fish;
   };
 
   home.sessionVariables = { };
@@ -53,7 +33,7 @@ in
   ];
 
   programs = {
-    # NOTE: nushell buggy on macOS still...
+    # NOTE: nushell buggy on macOS still - can't set as default shell
 
     # nushell = {
     #   enable = true;
