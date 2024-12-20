48ee0cf1fadde13a14a03d3afb91dff60e721f4a
Yasen Dimitrov <y_dimitrov@ymail.com>
2024-12-20 21:36:52 +0200

    nvim: cleanup blink config

--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -37,18 +37,6 @@ return {
       keymap = {
         ["<C-k>"] = { "select_prev", "fallback" },
         ["<C-j>"] = { "select_next", "fallback" },
-        -- ["<Tab>"] = {
-        --   function(cmp)
-        --     if cmp.is_in_snippet() then
-        --       return cmp.accept()
-        --     else
-        --       return cmp.select_and_accept()
-        --     end
-        --   end,
-        --   "snippet_forward",
-        --   "fallback",
-        -- },
-        -- ["<S-Tab>"] = { "snippet_backward", "fallback" },
       },
     },
   },
