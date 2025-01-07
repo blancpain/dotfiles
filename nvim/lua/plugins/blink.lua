41dd1f668ed9c28d33d93735c91a967d65077238
Yasen Dimitrov <y_dimitrov@ymail.com>
2025-01-07 11:21:53 +0200

    nvim: add colorful-menu and some blink style and keymap tweaks

--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -1,14 +1,80 @@
 return {
+  -- TODO: check if vim.tbl_deep_extend is necessary
   {
     "saghen/blink.cmp",
+    dependencies = { "xzbdmw/colorful-menu.nvim" },
+    opts = function(_, opts)
+      local colorful_menu = require("colorful-menu")
 
-    ---@module 'blink.cmp'
-    ---@type blink.cmp.Config
-    opts = {
-      keymap = {
+      -- Merge keymap settings
+      opts.keymap = vim.tbl_deep_extend("force", opts.keymap or {}, {
+        preset = "enter",
+        ["<Tab>"] = {
+          function(cmp)
+            if cmp.snippet_active() then
+              return cmp.accept()
+            else
+              return require("blink.cmp").select_next()
+            end
+          end,
+          "snippet_forward",
+          "fallback",
+        },
+        ["<S-Tab>"] = {
+          "select_prev",
+          "snippet_forward",
+          "fallback",
+        },
         ["<C-k>"] = { "select_prev", "fallback" },
         ["<C-j>"] = { "select_next", "fallback" },
-      },
-    },
+      })
+
+      -- Merge signature settings
+      opts.signature = vim.tbl_deep_extend("force", opts.signature or {}, {
+        enabled = true,
+        window = {
+          border = "rounded",
+        },
+      })
+
+      -- Merge appearance settings
+      opts.appearance = vim.tbl_deep_extend("force", opts.appearance or {}, {
+        kind_icons = {
+          Snippet = "",
+        },
+      })
+
+      -- Merge completion settings
+      opts.completion = vim.tbl_deep_extend("force", opts.completion or {}, {
+        menu = {
+          min_width = 20,
+          border = "rounded",
+          winhighlight = "Normal:Normal,FloatBorder:FloatBorder,CursorLine:BlinkCmpMenuSelection,Search:None",
+          draw = {
+            columns = { { "kind_icon" }, { "label", gap = 1 }, { "source" } },
+            components = {
+              label = {
+                text = colorful_menu.blink_components_text,
+                highlight = colorful_menu.blink_components_highlight,
+              },
+              source = {
+                text = function(ctx)
+                  local map = {
+                    ["lsp"] = "[]",
+                    ["path"] = "[󰉋]",
+                    ["snippets"] = "[]",
+                  }
+
+                  return map[ctx.item.source_id]
+                end,
+                highlight = "BlinkCmpSource",
+              },
+            },
+          },
+        },
+      })
+
+      return opts
+    end,
   },
 }
