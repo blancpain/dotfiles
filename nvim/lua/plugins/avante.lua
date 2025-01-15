fa07ba4096c0a25c01f58f6e109635b8273ac2a3
Yasen Dimitrov <y_dimitrov@ymail.com>
2025-01-15 16:40:36 +0200

    nvim: update avante config (WIP)

--- nvim/lua/plugins/avante.lua
+++ nvim/lua/plugins/avante.lua
@@ -2,15 +2,17 @@ return {
   "yetone/avante.nvim",
   event = "VeryLazy",
   lazy = false,
-  version = false, -- set this if you want to always pull the latest change
+  version = false, -- set this to "*" if you want to always pull the latest change, false to update on release
+  build = "make",
   opts = {
-    -- add any opts here
+    file_selector = {
+      provider = "fzf",
+      provider_opts = {},
+    },
   },
   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-  build = "make",
   -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
   dependencies = {
-    "nvim-treesitter/nvim-treesitter",
     "stevearc/dressing.nvim",
     "nvim-lua/plenary.nvim",
     "MunifTanjim/nui.nvim",
@@ -31,6 +33,35 @@ return {
           },
           -- required for Windows users
           use_absolute_path = true,
+        },
+      },
+    },
+    {
+      -- Make sure to set this up properly if you have lazy=true
+      "MeanderingProgrammer/render-markdown.nvim",
+      opts = {
+        file_types = { "markdown", "Avante" },
+      },
+      ft = { "markdown", "Avante" },
+    },
+    {
+      "saghen/blink.compat",
+      lazy = true,
+      opts = {},
+      config = function()
+        -- monkeypatch cmp.ConfirmBehavior for Avante
+        require("cmp").ConfirmBehavior = {
+          Insert = "insert",
+          Replace = "replace",
+        }
+      end,
+    },
+    {
+      "saghen/blink.cmp",
+      lazy = true,
+      opts = {
+        sources = {
+          compat = { "avante_commands", "avante_mentions", "avante_files" },
         },
       },
     },
