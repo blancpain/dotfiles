8c662f38c226ebedc64fa05352aee4efe6ebe110
Yasen Dimitrov <y_dimitrov@ymail.com>
2025-01-17 13:46:23 +0200

    nvim: lsp config refactor to allow keymap changes

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,7 +1,5 @@
-local util = require("lspconfig.util")
-
+-- local util = require("lspconfig.util")
 return {
-  -- tools
   {
     "williamboman/mason.nvim",
     opts = function(_, opts)
@@ -15,43 +13,62 @@ return {
   },
   {
     "neovim/nvim-lspconfig",
-    opts = {
-      inlay_hints = {
+    opts = function(_, opts)
+      -- Get the default keymaps
+      local keys = require("lazyvim.plugins.lsp.keymaps").get()
+
+      -- Modify keymaps here as per docs
+      keys[#keys + 1] = { "<c-k>", false }
+      keys[#keys + 1] = {
+        "<c-m>",
+        function()
+          return vim.lsp.buf.signature_help()
+        end,
+        mode = "i",
+        desc = "Signature Help",
+        has = "signatureHelp",
+      }
+
+      -- Extend the default options
+      opts.keys = keys
+      opts.inlay_hints = vim.tbl_deep_extend("force", opts.inlay_hints or {}, {
         enabled = true,
         exclude = { "javascript", "typescript", "javascriptreact", "typescriptreact" },
-      },
-      capabilities = {
+      })
+      opts.capabilities = vim.tbl_deep_extend("force", opts.capabilities or {}, {
         workspace = {
           didChangeWatchedFiles = {
             dynamicRegistration = true, -- NOTE: to fix having to restart lsp when adding/removing files
           },
         },
-      },
-      diagnostics = {
+      })
+      opts.diagnostics = vim.tbl_deep_extend("force", opts.diagnostics or {}, {
         virtual_text = false,
-      },
-      servers = {
+      })
+
+      -- Extend/override the servers table
+      opts.servers = vim.tbl_deep_extend("force", opts.servers or {}, {
         -- TODO: see if below needed as inc in extras now
-        volar = {
-          filetypes = {
-            "typescript",
-            "vue",
-          },
-          root_dir = util.root_pattern("src/App.vue"),
-          languageFeatures = {
-            implementation = true,
-            references = true,
-            definition = true,
-            typeDefinition = true,
-            callHierarchy = true,
-            hover = true,
-            rename = true,
-            renameFileRefactoring = true,
-            signatureHelp = true,
-            codeAction = true,
-            workspaceSymbol = true,
-          },
-        },
+        -- volar = {
+        --   filetypes = {
+        --     "typescript",
+        --     "vue",
+        --   },
+        --   root_dir = util.root_pattern("src/App.vue"),
+        --   languageFeatures = {
+        --     implementation = true,
+        --     references = true,
+        --     definition = true,
+        --     typeDefinition = true,
+        --     callHierarchy = true,
+        --     hover = true,
+        --     rename = true,
+        --     renameFileRefactoring = true,
+        --     signatureHelp = true,
+        --     codeAction = true,
+        --     workspaceSymbol = true,
+        --   },
+        -- },
         vtsls = {
           settings = {
             typescript = {
@@ -83,7 +100,8 @@ return {
             "typescriptreact",
           },
         },
-      },
-    },
+      })
+      return opts
+    end,
   },
 }
