3970a400991943ceff0927f14274039c8eba4857
Yasen Dimitrov <y_dimitrov@ymail.com>
2025-01-26 21:06:07 +0200

    nvim: cleanup

--- nvim/lua/plugins/avante.lua
+++ nvim/lua/plugins/avante.lua
@@ -15,38 +15,6 @@ return {
         start_insert = false,
       },
     },
-    mappings = {
-      --- @class AvanteConflictMappings
-      -- diff = {
-      --   ours = "co",
-      --   theirs = "ct",
-      --   all_theirs = "ca",
-      --   both = "cb",
-      --   cursor = "cc",
-      --   next = "]x",
-      --   prev = "[x",
-      -- },
-      -- suggestion = {
-      --   accept = "<M-l>",
-      --   next = "<M-]>",
-      --   prev = "<M-[>",
-      --   dismiss = "<C-]>",
-      -- },
-      -- jump = {
-      --   next = "]]",
-      --   prev = "[[",
-      -- },
-      -- submit = {
-      --   normal = "<CR>",
-      --   insert = "<C-s>",
-      -- },
-      -- sidebar = {
-      --   apply_all = "A",
-      --   apply_cursor = "a",
-      --   switch_windows = "<Tab>",
-      --   reverse_switch_windows = "<S-Tab>",
-      -- },
-    },
   },
   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
   -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
