f76b9fb179919caee5c41846d4f3952f9009788d
Yasen Dimitrov <y_dimitrov@ymail.com>
2025-03-07 14:13:22 +0200

    nvim: avante go back to claude default

--- nvim/lua/plugins/avante.lua
+++ nvim/lua/plugins/avante.lua
@@ -6,26 +6,15 @@ return {
   build = "make",
   opts = {
     -- NOTE: below experimental for deepseek but it means that deepseek will be the default
-    -- provider = "openai",
-    -- auto_suggestions_provider = "openai", -- Since auto-suggestions are a high-frequency operation and therefore expensive, it is recommended to specify an inexpensive provider or even a free provider: copilot
-    -- openai = {
-    --   endpoint = "https://api.deepseek.com",
-    --   model = "deepseek-chat",
-    --   timeout = 30000, -- Timeout in milliseconds
-    --   temperature = 0,
-    --   max_tokens = 4096,
-    --   -- optional
-    --   api_key_name = "DEEPSEEK_API_KEY",
+    -- provider = "deepseek",
+    -- vendors = {
+    --   deepseek = {
+    --     __inherited_from = "openai",
+    --     api_key_name = "DEEPSEEK_API_KEY",
+    --     endpoint = "https://api.deepseek.com",
+    --     model = "deepseek-coder",
+    --   },
     -- },
-    provider = "deepseek",
-    vendors = {
-      deepseek = {
-        __inherited_from = "openai",
-        api_key_name = "DEEPSEEK_API_KEY",
-        endpoint = "https://api.deepseek.com",
-        model = "deepseek-coder",
-      },
-    },
     file_selector = {
       provider = "snacks",
       provider_opts = {},
