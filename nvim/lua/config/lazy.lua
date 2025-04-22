2175c7c2b6d858594c5eb848bb7e985bd6843306
Yasen Dimitrov <y_dimitrov@ymail.com>
2025-04-22 14:54:55 +0300

    nvim: add python

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -17,6 +17,7 @@ local spec = {
   { import = "lazyvim.plugins.extras.lang.svelte" },
   { import = "lazyvim.plugins.extras.lang.toml" },
   { import = "lazyvim.plugins.extras.lang.rust" },
+  { import = "lazyvim.plugins.extras.lang.python" },
   { import = "lazyvim.plugins.extras.lang.nix" },
   { import = "lazyvim.plugins.extras.lang.nushell" },
   { import = "lazyvim.plugins.extras.formatting.prettier" },
