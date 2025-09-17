5b9360425184259551ab24adc9251a73cb4cf009
Yasen Dimitrov <y_dimitrov@ymail.com>
2025-09-17 17:46:08 +0300

    nvim: cleanup unused files, update lazyvim

--- nvim/lua/plugins/mason.lua
+++
@@ -1,5 +0,0 @@
--- FIX: temp workaround while breaking changes due to mason v2.0.0 are not resolved
-return {
-  { "mason-org/mason.nvim", version = "1.11.0" },
-  { "mason-org/mason-lspconfig.nvim", version = "1.32.0" },
-}
