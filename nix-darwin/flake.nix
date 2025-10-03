bc2dfc4fadd0a6045dd32a418ca6183f33b3bb11
Yasen Dimitrov <y_dimitrov@ymail.com>
2025-10-03 10:28:18 +0300

    nix: add system.primaryUser

--- nix-darwin/flake.nix
+++ nix-darwin/flake.nix
@@ -94,6 +94,8 @@
           # Set Git commit hash for darwin-version.
           system.configurationRevision = self.rev or self.dirtyRev or null;
 
+          system.primaryUser = "blancpain";
+
           # Used for backwards compatibility, please read the changelog before changing.
           # $ darwin-rebuild changelog
           system.stateVersion = 5;
