8104e2b16cfd1349d5b6fce3673e57efd4566d78
Yasen Dimitrov <y_dimitrov@ymail.com>
2025-10-14 17:04:40 +0300

    nvim: replace obsolete obsidian plugin with new community supported version

--- nvim/lua/plugins/obsidian.lua
+++ nvim/lua/plugins/obsidian.lua
@@ -1,30 +1,18 @@
 return {
-  "epwalsh/obsidian.nvim",
-  version = "*", -- Use latest release instead of latest commit
-  lazy = true,
+  "obsidian-nvim/obsidian.nvim",
+  version = "*", -- recommended, use latest release instead of latest commit
   ft = "markdown",
   -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
   -- event = {
   --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
-  --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
-  --   "BufReadPre path/to/my-vault/**.md",
-  --   "BufNewFile path/to/my-vault/**.md",
+  --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
+  --   -- refer to `:h file-pattern` for more examples
+  --   "BufReadPre path/to/my-vault/*.md",
+  --   "BufNewFile path/to/my-vault/*.md",
   -- },
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-  },
-
+  ---@module 'obsidian'
+  ---@type obsidian.config
   opts = {
-    -- ui = {
-    -- checkboxes = {},
-    -- bullets = {},
-    -- external_link_icon = {},
-    -- },
-
-    ui = {
-      enable = false,
-    },
-
     workspaces = {
       {
         name = "obsidian",
@@ -32,60 +20,16 @@ return {
       },
     },
 
-    -- completion = {
-    --   nvim_cmp = true,
-    --   min_chars = 2,
-    -- },
-    new_notes_location = "current_dir",
-    wiki_link_func = function(opts)
-      if opts.id == nil then
-        return string.format("[[%s]]", opts.label)
-      elseif opts.label ~= opts.id then
-        return string.format("[[%s|%s]]", opts.id, opts.label)
-      else
-        return string.format("[[%s]]", opts.id)
-      end
-    end,
-
-    note_frontmatter_func = function(note)
-      -- This is equivalent to the default frontmatter function.
-      local out = { id = note.id, aliases = note.aliases, tags = note.tags, area = "", project = "" }
-
-      -- `note.metadata` contains any manually added fields in the frontmatter.
-      -- So here we just make sure those fields are kept in the frontmatter.
-      if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
-        for k, v in pairs(note.metadata) do
-          out[k] = v
-        end
-      end
-      return out
-    end,
-
-    note_id_func = function(title)
-      -- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
-      -- In this case a note with the title 'My new note' will be given an ID that looks
-      -- like '1657296016-my-new-note', and therefore the file name '1657296016-my-new-note.md'
-      local suffix = ""
-      if title ~= nil then
-        -- If title is given, transform it into valid file name.
-        suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
-      else
-        -- If title is nil, just add 4 random uppercase letters to the suffix.
-        for _ = 1, 4 do
-          suffix = suffix .. string.char(math.random(65, 90))
-        end
-      end
-      return tostring(os.time()) .. "-" .. suffix
-    end,
-
-    templates = {
-      subdir = "Templates",
-      date_format = "%Y-%m-%d-%a",
-      time_format = "%H:%M",
-      tags = "",
+    ui = {
+      enable = false,
     },
-  },
 
+    picker = {
+      name = Snacks,
+    },
+
+    -- see below for full list of options 👇
+  },
   keys = {
     { "<leader>on", "<cmd>ObsidianNew<cr>", desc = "New Obsidian note", mode = "n" },
     { "<leader>oo", "<cmd>ObsidianSearch<cr>", desc = "Search Obsidian notes", mode = "n" },
@@ -94,3 +38,78 @@ return {
     { "<leader>ot", "<cmd>ObsidianTemplate<cr>", desc = "Follow link under cursor", mode = "n" },
   },
 }
+-- return {
+--   "epwalsh/obsidian.nvim",
+--   version = "*", -- Use latest release instead of latest commit
+--   lazy = true,
+--   dependencies = {
+--     "nvim-lua/plenary.nvim",
+--   },
+--
+--   opts = {
+--     -- ui = {
+--     -- checkboxes = {},
+--     -- bullets = {},
+--     -- external_link_icon = {},
+--     -- },
+--
+--     ui = {
+--       enable = false,
+--     },
+--
+--
+--     -- completion = {
+--     --   nvim_cmp = true,
+--     --   min_chars = 2,
+--     -- },
+--     new_notes_location = "current_dir",
+--     wiki_link_func = function(opts)
+--       if opts.id == nil then
+--         return string.format("[[%s]]", opts.label)
+--       elseif opts.label ~= opts.id then
+--         return string.format("[[%s|%s]]", opts.id, opts.label)
+--       else
+--         return string.format("[[%s]]", opts.id)
+--       end
+--     end,
+--
+--     note_frontmatter_func = function(note)
+--       -- This is equivalent to the default frontmatter function.
+--       local out = { id = note.id, aliases = note.aliases, tags = note.tags, area = "", project = "" }
+--
+--       -- `note.metadata` contains any manually added fields in the frontmatter.
+--       -- So here we just make sure those fields are kept in the frontmatter.
+--       if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
+--         for k, v in pairs(note.metadata) do
+--           out[k] = v
+--         end
+--       end
+--       return out
+--     end,
+--
+--     note_id_func = function(title)
+--       -- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
+--       -- In this case a note with the title 'My new note' will be given an ID that looks
+--       -- like '1657296016-my-new-note', and therefore the file name '1657296016-my-new-note.md'
+--       local suffix = ""
+--       if title ~= nil then
+--         -- If title is given, transform it into valid file name.
+--         suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
+--       else
+--         -- If title is nil, just add 4 random uppercase letters to the suffix.
+--         for _ = 1, 4 do
+--           suffix = suffix .. string.char(math.random(65, 90))
+--         end
+--       end
+--       return tostring(os.time()) .. "-" .. suffix
+--     end,
+--
+--     templates = {
+--       subdir = "Templates",
+--       date_format = "%Y-%m-%d-%a",
+--       time_format = "%H:%M",
+--       tags = "",
+--     },
+--   },
+--
+-- }
