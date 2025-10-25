620668d6bf67b64bf3ba0dddf4b9369e47147db6
Yasen Dimitrov <y_dimitrov@ymail.com>
2025-10-25 23:17:20 +0300

    nvim: lsp update keys config to match new api

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -15,38 +15,38 @@ return {
   -- },
   {
     "neovim/nvim-lspconfig",
-    init = function()
-      --[[Modify LSP keymaps]]
-      local keys = require("lazyvim.plugins.lsp.keymaps").get()
-      -- NOTE: make sure you include "mode" as without it disabling doesn't seem to work
-      -- see: https://github.com/dpetka2001/dotfiles/blob/main/dot_config/nvim/lua/plugins/lsp.lua
-      keys[#keys + 1] = { "<C-k>", false, mode = { "i" } }
-      keys[#keys + 1] = {
-        "<C-g>",
-        function()
-          return vim.lsp.buf.signature_help()
-        end,
-        mode = { "i" },
-        desc = "Signature Help",
-        has = "signatureHelp",
-      }
-    end,
     opts = {
       inlay_hints = {
         enabled = true,
         exclude = { "javascript", "typescript", "javascriptreact", "typescriptreact" },
       },
-      capabilities = {
-        workspace = {
-          didChangeWatchedFiles = {
-            dynamicRegistration = true, -- NOTE: to fix having to restart lsp when adding/removing files
-          },
-        },
-      },
       diagnostics = {
         virtual_text = false,
       },
       servers = {
+        ["*"] = {
+          capabilities = {
+            workspace = {
+              didChangeWatchedFiles = {
+                dynamicRegistration = true, -- NOTE: to fix having to restart lsp when adding/removing files
+              },
+            },
+          },
+          keys = {
+            -- Disable <C-k> in insert mode
+            { "<C-k>", false, mode = "i" },
+            -- Add custom signature help mapping
+            {
+              "<C-g>",
+              function()
+                return vim.lsp.buf.signature_help()
+              end,
+              mode = "i",
+              desc = "Signature Help",
+              has = "signatureHelp",
+            },
+          },
+        },
         -- TODO: see if below needed as inc in extras now
         volar = {
           filetypes = {
