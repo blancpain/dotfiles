989927a9a91ef94a1c0ac22d5e1cade486c58748
Yasen Dimitrov <y_dimitrov@ymail.com>
2025-11-10 22:51:37 +0200

    nvim: rm auto preview cmd oil and cleanup plugin spec

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -7,20 +7,12 @@ function M.config()
   require("oil").setup({
     float = {
       max_height = 40,
-      max_width = 150,
-      -- padding = 20,
+      max_width = 100,
       border = "rounded",
     },
-    -- columns = {
-    --   "icon",
-    --   "size",
-    -- },
     keymaps = {
       ["<C-h>"] = false,
     },
-    -- win_options = {
-    --   winbar = "%{v:lua.require('oil').get_current_dir()}", -- show current directory in winbar
-    -- },
     view_options = {
       show_hidden = true,
       is_always_hidden = function(name, _)
@@ -28,17 +20,12 @@ function M.config()
       end,
     },
     lsp_file_methods = {
-      -- Enable or disable LSP file operations
       enabled = true,
-      -- Time to wait for LSP file operations to complete before skipping
       timeout_ms = 1000,
-      -- Set to true to autosave buffers that are updated with LSP willRenameFiles
-      -- Set to "unmodified" to only save unmodified buffers
       autosave_changes = true,
     },
   })
   vim.keymap.set("n", "-", "<CMD>Oil --float<CR>", { desc = "Open parent directory" })
-  -- vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 end
 
 return M
