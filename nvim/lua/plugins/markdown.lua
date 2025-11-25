5c82a79716069134f21875ed44a304e6fba5dbc9
Yasen Dimitrov <y_dimitrov@ymail.com>
2025-11-25 14:28:38 +0200

    nvim: add taskman

--- nvim/lua/plugins/markdown.lua
+++ nvim/lua/plugins/markdown.lua
@@ -1,17 +1,28 @@
 return {
-  "MeanderingProgrammer/render-markdown.nvim",
-  opts = {
-    file_types = {
-      "markdown",
-      "norg",
-      "rmd",
-      "org",
+  {
+
+    "MeanderingProgrammer/render-markdown.nvim",
+    opts = {
+      file_types = {
+        "markdown",
+        "norg",
+        "rmd",
+        "org",
+      },
+      bullet = {
+        -- padding
+        left_pad = 0,
+        right_pad = 1,
+      },
     },
-    bullet = {
-      -- padding
-      left_pad = 0,
-      right_pad = 1,
+    ft = { "markdown", "norg", "rmd", "org" },
+  },
+
+  {
+    "Jonathan-Al-Saadi/taskman.nvim",
+    opts = {
+      -- Directory to search for markdown task files
+      task_dir = "~/Library/Mobile Documents/iCloud~md~obsidian/Documents/obsidian",
     },
   },
-  ft = { "markdown", "norg", "rmd", "org" },
 }
