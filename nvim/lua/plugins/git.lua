fa5df14b4df5b4f927f5576dc239f466b40ff9e0
Yasen Dimitrov <y_dimitrov@ymail.com>
2025-11-30 15:01:07 +0200

    nvim: rm neogit

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -5,27 +5,4 @@ return {
   { "lewis6991/gitsigns.nvim", opts = {
     current_line_blame = true,
   } },
-  {
-    "NeogitOrg/neogit",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "sindrets/diffview.nvim",
-      "folke/snacks.nvim",
-    },
-    opts = {
-      signs = {
-        hunk = { "", "" },
-        item = { "", "" },
-        section = { "", "" },
-      },
-      integrations = {
-        diffview = true,
-        snacks = true,
-      },
-      kind = "vsplit",
-    },
-    keys = {
-      { "<leader>gn", "<cmd>lua require('neogit').open()<CR>", desc = "Neogit Status" },
-    },
-  },
 }
