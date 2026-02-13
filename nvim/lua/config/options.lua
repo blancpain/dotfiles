6e2abf752e1f2f84702f062ddbf889f8e5bf6cdd
Yasen Dimitrov <y_dimitrov@ymail.com>
2026-02-13 22:09:16 +0200

    nvim: make fish path agnostic in nvim options

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -3,7 +3,7 @@ vim.o.swapfile = false -- disable swapfile
 vim.opt.updatetime = 100 -- faster completion (4000ms default)
 vim.opt.timeoutlen = 300
 vim.o.background = "dark" -- dark vs light
-vim.o.shell = "/run/current-system/sw/bin/fish" -- use fish shell
+vim.o.shell = vim.fn.exepath("fish") or "fish" -- use fish shell
 -- Add asterisks in block comments
 vim.opt.formatoptions:append({ "r" }) -- WARN: doesn't work as expected
 vim.opt.clipboard = "unnamedplus"
