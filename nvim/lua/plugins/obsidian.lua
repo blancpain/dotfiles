0bf6b22164e5384d2184437744d16afd6f9291ed
Yasen Dimitrov <yasen.dimitrov@dataart.com>
2026-03-26 16:15:55 +0200

    nvim: obsidian formatting tweaks and new note creation changes

--- nvim/lua/plugins/obsidian.lua
+++ nvim/lua/plugins/obsidian.lua
@@ -21,10 +21,38 @@ return {
       name = Snacks,
     },
 
-    -- see below for full list of options 👇
+    note_id_func = function(title)
+      return title
+    end,
+
+    frontmatter = {
+      func = function(note)
+        local id = tostring(note.id)
+        if not id:match("^%d+%-") then
+          id = tostring(os.time()) .. "-" .. id
+        end
+        local out = { id = id, aliases = note.aliases, tags = note.tags, area = {}, project = {} }
+        if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
+          for k, v in pairs(note.metadata) do
+            out[k] = v
+          end
+        end
+        return out
+      end,
+    },
   },
   keys = {
-    { "<leader>on", "<cmd>Obsidian new<cr>", desc = "New Obsidian note", mode = "n" },
+    {
+      "<leader>on",
+      function()
+        local title = vim.fn.input("Note title: ")
+        if title ~= "" then
+          vim.cmd("Obsidian new " .. title)
+        end
+      end,
+      desc = "New Obsidian note",
+      mode = "n",
+    },
     { "<leader>oo", "<cmd>Obsidian search<cr>", desc = "Search Obsidian notes", mode = "n" },
     { "<leader>os", "<cmd>Obsidian quick_switch<cr>", desc = "Quick Switch", mode = "n" },
     { "<leader>ob", "<cmd>Obsidian backlinks<cr>", desc = "Show location list of backlinks", mode = "n" },
