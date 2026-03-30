d8f6bb77ddf4f31a6a745f65c866260bdc312a86
Yasen Dimitrov <yasen.dimitrov@dataart.com>
2026-03-30 17:21:17 +0300

    nvim: replace diffview with codediff

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -8,4 +8,11 @@ return {
   { "lewis6991/gitsigns.nvim", opts = {
     current_line_blame = true,
   } },
+  {
+    "esmuellert/codediff.nvim",
+    cmd = "CodeDiff",
+    keys = {
+      { "<leader>gk", "<cmd>CodeDiff<cr>", desc = "Git Diff" },
+    },
+  },
 }
